870fc92867bed150745a38ec9b99013f07c2acea
Pankaj1662005 <123957110+Pankaj1662005@users.noreply.github.com>
2025-01-20 00:27:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version="0.1.0",
     author="Pankaj",
     author_email="pankajsheokand2005@gmail.com",
-    description="A simple Python package which we can use  for TOPSIS analysis. This is created by Pankaj Sheokand student of thapar university ",
+    description="A simple Python package for performing TOPSIS analysis. Created by Pankaj Sheokand.",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
     entry_points={
         "console_scripts": [
-            "topsis=pankaj_102203410_topsis.cli:main",
+            "topsis=topsis_toolkit.cli:main",
         ],
     },
     python_requires=">=3.6",
